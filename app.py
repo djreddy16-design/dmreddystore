67dceaa26b90247eddee99c88b98b450733c5f43
djreddy16-design <djreddy16@gmail.com>
2025-12-23 16:06:01 +0530

    Fix port for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
@@ -79,4 +80,5 @@ def edit_crop(id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
